aab5b9959e9787adc7a18fb7417a5c4e92b9e3e5
mustafaomereser <mustafaomereser@gmail.com>
2026-03-22 06:50:36 +0300

    added test page

--- App.php
+++ App.php
@@ -33,6 +33,52 @@ class App
             return $this->response("TEST OK: " . rand(), 200, $keepAlive);
         }
 
+        // POST / JSON echo
+        if ($req->uri === '/test-echo') {
+            $output = json_encode([
+                'method'       => $req->method,
+                'content_type' => $req->contentType,
+                '_POST'        => $_POST,
+                '_GET'         => $_GET,
+                'body_raw'     => $req->body,
+            ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            return $this->response($output, 200, $keepAlive, 'application/json');
+        }
+
+        // File upload echo
+        if ($req->uri === '/test-upload') {
+            $files = [];
+            foreach ($_FILES as $key => $file) {
+                if (is_array($file['name'])) {
+                    // files[] gibi array upload
+                    foreach ($file['name'] as $i => $name) {
+                        $files[] = [
+                            'field'    => $key . '[' . $i . ']',
+                            'name'     => $name,
+                            'type'     => $file['type'][$i],
+                            'size'     => $file['size'][$i],
+                            'error'    => $file['error'][$i],
+                            'tmp_name' => $file['tmp_name'][$i],
+                        ];
+                    }
+                } else {
+                    $files[] = [
+                        'field'    => $key,
+                        'name'     => $file['name'],
+                        'type'     => $file['type'],
+                        'size'     => $file['size'],
+                        'error'    => $file['error'],
+                        'tmp_name' => $file['tmp_name'],
+                    ];
+                }
+            }
+            $output = json_encode([
+                'files'  => $files,
+                '_POST'  => $_POST,
+            ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            return $this->response($output, 200, $keepAlive, 'application/json');
+        }
+
         return $this->response("404 Not Found", 404, $keepAlive);
     }
 
@@ -45,14 +91,14 @@ class App
         return $this->response($html, 200, $keepAlive);
     }
 
-    public function response(string $content, int $status = 200, bool $keepAlive = false): string
+    public function response(string $content, int $status = 200, bool $keepAlive = false, string $contentType = 'text/html; charset=utf-8'): string
     {
         $text       = self::STATUS_TEXTS[$status] ?? 'OK';
         $connection = $keepAlive ? 'keep-alive' : 'close';
         $extra      = $keepAlive ? "Keep-Alive: timeout=5, max=1000\r\n" : '';
 
         return "HTTP/1.1 $status $text\r\n" .
-               "Content-Type: text/html; charset=utf-8\r\n" .
+               "Content-Type: $contentType\r\n" .
                "Connection: $connection\r\n" .
                $extra .
                "Content-Length: " . strlen($content) . "\r\n" .
